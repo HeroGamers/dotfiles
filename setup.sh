011d71092015dc7050a1fb036d30c33155337c31
HeroGamers <hero@herogamers.dev>
2023-10-27 20:37:56 +0800

    Add pwninit, move scripts that should be run as current user to new file

--- setup.sh
+++ setup.sh
@@ -34,15 +34,6 @@ gem install one_gadget
 
 # Install CTF tools
 
-## pwndbg
-if [ ! -d "$HOME/pwndbg" ]; then
-    echo "Installing pwndbg..."
-    cd $HOME
-    git clone https://github.com/pwndbg/pwndbg.git
-    cd $HOME/pwndbg
-    ./setup.sh
-fi
-
 ## pathelf
 if [ ! -d "$HOME/patchelf" ]; then
     echo "Installing pathelf..."
@@ -56,54 +47,11 @@ if [ ! -d "$HOME/patchelf" ]; then
     make install
 fi
 
-# Other customizations
-
-## TPM (tmux plugin manager)
-if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
-    echo "Installing TPM..."
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-fi
-
-## NVchad
-if [ ! -d "$HOME/.config/nvim/lua/core" ]; then
-    echo "Installing NVChad..."
-	mkdir ~/tmp
-	mv ~/.config/nvim/lua/custom ~/tmp/nvim_custom
-    git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
-	mv ~/tmp/nvim_custom ~/.config/nvim/lua/custom
-fi
-
-## oh-my-zsh
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-    echo "Installing oh-my-zsh..."
-    # We want to keep zshrc and run it unattended
-    KEEP_ZSHRC=yes
-    RUNZSH=no
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
-fi
-
-## zsh dependencies
-
-### powerlevel10k theme
-if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
-    echo "Downloading powerlevel10k..."
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-fi
-
-### Fast syntax highlighting
-if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/F-Sy-H" ]; then
-    echo "Downloading fast-syntax-highlighting..."
-    git clone https://github.com/z-shell/F-Sy-H.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/F-Sy-H
-fi
-
-### Autocomplete
-if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autocomplete" ]; then
-    echo "Downloading zsh-autocomplete..."
-    git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autocomplete
-fi
-
-### Autosuggest
-if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
-    echo "Downloading zsh-autosuggestions..."
-    git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# pwninit
+if [ ! -f "/usr/local/bin/pwninit" ]; then
+    echo "Installing pwninit..."
+    cd /usr/local/bin
+	wget https://github.com/io12/pwninit/releases/latest/download/pwninit
+	chmod 755 ./pwninit
+	cd $HOME
 fi
