ca0f2c62733b358e474551a91817d3a44b5cf406
HeroGamers <hero@herogamers.dev>
2023-11-09 15:30:06 +0800

    New packages

--- setup.sh
+++ setup.sh
@@ -22,14 +22,24 @@ apt -y install software-properties-common python3-launchpadlib
 #add-apt-repository ppa:neovim-ppa/stable
 
 ### Install stuff :D
-apt -y install git tmux neovim fish htop ranger wget curl binutils nasm gcc-multilib g++-multilib libc6-dev-i386 libc6-dbg nmap libssl-dev libffi-dev gdb build-essential ltrace strace ruby-rubygems python3 python3-gmpy2 python3-pip python3-dev ruby-full netcat-traditional autoconf libtool automake zsh-autosuggestions zsh-syntax-highlighting zsh
+apt -y install git tmux neovim fish htop ranger wget curl binutils nasm gcc-multilib g++-multilib libc6-dev-i386 libc6-dbg nmap libssl-dev libffi-dev gdb build-essential ltrace strace ruby-rubygems python3 python3-gmpy2 python3-pip python3-dev python3-setuptools ruby-full netcat-traditional autoconf libtool automake zsh-autosuggestions zsh-syntax-highlighting zsh tldr bat ffmpeg imagemagick ncdu
 # netcat => netcat-traditional     skipped libc6-dbg:i386 and lazygit
 
 # Update Python pip packages and install needed packages
+
+## Upgrade pip
 pip install --upgrade pip
-pip install --upgrade capstone pyelftools==0.29 pycryptodome
-pip install --upgrade --ignore-installed pwntools
-pip install --upgrade ropgadget
+
+## Upgrade setuptools
+pip install --upgrade setuptools
+
+## Needed packages globally
+pip install --upgrade numpy pandas scipy sympy Pillow asyncio aiohttp beautifulsoup4 thefuck
+
+## CTF stuff
+pip install --upgrade capstone pyelftools==0.29 pycryptodome pwntools ropgadet gmpy2 z3-solver angr
+#pip install --upgrade --ignore-installed pwntools
+#pip install --upgrade ropgadget
 gem install one_gadget
 
 # Install CTF tools
