922c938d99d3f3e155675a4019bcac15dc12d41b
Hero <hero@herogamers.dev>
2024-09-18 12:02:51 +0200

    fix: try to fix monitors, it still brokey, but oh well

--- home-manager/hacktop/monitors.nix
+++ home-manager/hacktop/monitors.nix
@@ -6,9 +6,9 @@
     wayland.windowManager.hyprland.settings = {
         monitor = [
             # Docking Station
-            "desc:Philips Consumer Electronics Company PHL 275E2F UHB2247007016, preferred, 0x0, auto"
-            "desc:Philips Consumer Electronics Company PHL 275E2F UHB2247007014, preferred, 2560x0, auto"
-            "desc:Philips Consumer Electronics Company PHL 275E2F UHB2247007017, preferred, 5120x0, auto"
+            "desc:Philips Consumer Electronics Company PHL 275E2F UHB2247007016, preferred, 0x0, 1"
+            "desc:Philips Consumer Electronics Company PHL 275E2F UHB2247007014, preferred, 2560x0, 1"
+            "desc:Philips Consumer Electronics Company PHL 275E2F UHB2247007017, preferred, 5120x0, 1"
         ];
     };
 }
