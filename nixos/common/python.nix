04c5e3e637f0126f7f6c6d7369523cdbb01ba3ef
Hero <hero@herogamers.dev>
2024-11-24 19:04:22 +0100

    fix: Python fix

--- nixos/common/python.nix
+++ nixos/common/python.nix
@@ -7,17 +7,24 @@
 }: {
   environment.systemPackages = with pkgs; [
     (python3.withPackages (python-pkgs: [
+      python-pkgs.aiofiles
       python-pkgs.aiohttp
       python-pkgs.beautifulsoup4
+      python-pkgs.black
+      python-pkgs.fastapi
+      python-pkgs.flake8
+      python-pkgs.jinja2
       python-pkgs.matplotlib
       python-pkgs.numpy
       python-pkgs.pandas
       python-pkgs.pillow
       python-pkgs.pyaudio
+      python-pkgs.pydantic
       python-pkgs.requests
       python-pkgs.scipy
       python-pkgs.sympy
       python-pkgs.tkinter
+      python-pkgs.uvicorn
 
       # Security
       #python-pkgs.angr # currently broken on nixpkgs
