299736518df8df08a0b65b1e10f1ae43c710b8a6
Marcus Sand <hero@herogamers.dev>
2026-01-13 12:58:57 +0100

    fix(binwalk): point to the correct binwalk package

--- pkgs/by-name/bi/binwalk2/default.nix
+++ pkgs/by-name/bi/binwalk2/default.nix
@@ -2,7 +2,7 @@
   python3Packages,
 }:
 let
-  binwalk = with python3Packages; callPackage ../development/python-modules/binwalk { };
+  binwalk = with python3Packages; callPackage ../../../development/python-modules/binwalk { };
 in
 
 with python3Packages;
