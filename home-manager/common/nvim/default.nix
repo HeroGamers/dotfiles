38ff9cb6719ef965c74943667029149ea905447e
Hero <hero@herogamers.dev>
2026-07-19 16:23:06 +0200

    fix: other eval warnings, other than minegrub and xorg

--- home-manager/common/nvim/default.nix
+++ home-manager/common/nvim/default.nix
@@ -6,6 +6,8 @@
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
+    withRuby = false;
+    withPython3 = false;
 
     # extraLuaConfig = ''
     #   ${builtins.readFile ./options.lua}
